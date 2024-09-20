48290c1ce81eb0cb480d76ddeebdbec4d1abbe49
jess <kittensealin@gmail.com>
2024-09-20 11:35:17 +0800

    add test account info on sign up page

--- public/javascript/sign.js
+++ public/javascript/sign.js
@@ -12,6 +12,8 @@ import {
 back_Homepage();
 go_signpage();
 search();
+document.querySelector("#signinEmail").value = "test@test.com";
+document.querySelector("#signinPassword").value = "123456";
 
 const token = localStorage.getItem("token");
 if (token) {
